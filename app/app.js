f72ba1427582cce54534c120f317a6bd6ccbb761
Thiago Galesi <thiagogalesi@gmail.com>
2016-09-25 17:19:06 +0100

    Basic contact list

--- app/app.js
+++ app/app.js
@@ -1,14 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('ContactsApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ContactsApp.services',
+  'ContactsApp.contacts',
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/contacts'});
 }]);
